fe512b66f8018bd263b0d225e16e94f7667351c2
vgridniev <vgridniev@bears.software>
2022-06-09 20:10:46 +0300

    first commit

---
+++ src/main.js
@@ -0,0 +1,16 @@
+import Vue from 'vue'
+import App from './App.vue'
+
+import store from './store'
+import axios from './axios'
+import VueAxios from 'vue-axios'
+import VueCookies from 'vue-cookies'
+
+Vue.use(VueAxios, axios)
+Vue.use(VueCookies)
+Vue.use(store)
+
+new Vue({
+    router,
+    render: h => h(App),
+}).$mount('#app')
