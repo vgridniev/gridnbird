3508e521bba9f496e223caebb5db6374c9c6ccee
vgridniev <vgridniev@bears.software>
2022-06-09 20:25:41 +0300

    fix  first launch

--- src/main.js
+++ src/main.js
@@ -1,10 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
 
-import store from './store'
-import axios from './axios'
-import VueAxios from 'vue-axios'
-import VueCookies from 'vue-cookies'
+// import store from './store'
+// import axios from './axios'
+// import VueAxios from 'vue-axios'
+// import VueCookies from 'vue-cookies'
 
 Vue.use(VueAxios, axios)
 Vue.use(VueCookies)
